ad797f8c05cf1cf22b4c7e3a4ee232d1046e179a
Nimesh <nimeshnott@hotmail.co.uk>
2020-03-16 00:54:27 +0000

    footer added

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,29 +1,24 @@
 import React from 'react'
-// import Image from 'gatsby-image'
+import { StaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
 import "../utils/css/components/hero.scss"
-// import { graphql } from "gatsby"
-
-// import "..css/components/hero.css"
 
 const hero = () => {
     return (
     <div className="container">
-        <h1>I am a dickhead</h1>
         <div className="hero-image">
-            {/* <Img /> */}
+        {/* <Image /> */}
         </div>
         <div className="intro">
-            <h3>Brief</h3>
+            <h4>Brief</h4>
             <p>in this project wanted to eradicate wallets</p>
         </div>
     </div>
     );
 }
 
-export default hero;
-
-// const bioQuery = graphql`
-//   query BioQuery {
+// const heroImage = graphql`
+//   query heroImage {
 //     avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
 //       childImageSharp {
 //         fixed(width: 50, height: 50) {
@@ -40,4 +35,7 @@ export default hero;
 //       }
 //     }
 //   }
-// 
+// `
+
+
+export default hero;
