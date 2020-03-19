e93bc9c1fc2a3b7383e35582d005d48ea479e1e5
Nimesh <nimeshnott@hotmail.co.uk>
2020-03-19 21:53:47 +0000

    start gallery + 70% contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,42 +13,54 @@ const AboutPage = ({ data }, location) => {
 
   return (
     <Layout title={siteTitle}>
-      <SEO title="ABout" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
+      <SEO
+        title="Contact"
+        keywords={[`product`, `designer`, `london`, `nimesh`]}
+      />
 
-      <article className="post-content page-template no-image">
-        <div className="post-content-body">
-          <h2 id="clean-minimal-and-deeply-customisable-london-is-a-theme-made-for-people-who-appreciate-simple-lines-">
-            Clean, minimal, and deeply customisable. London is a theme made for
-            people who appreciate simple lines.
-          </h2>
-          <figure className="kg-card kg-image-card kg-width-full">
-            <Img
-              fluid={data.benchAccounting.childImageSharp.fluid}
-              className="kg-image"
-            />
-            <figcaption>Large imagery is at the heart of this theme</figcaption>
-          </figure>
-          <h3 id="dynamic-styles">Dynamic styles</h3>
-          <p>
-            London comes with photo-centric main layout best suited to
-            photography, graphics portfolios and other image-heavy uses.
-          </p>
-          <p>
-            Both post and page templates are light and minimal, with all the
-            focus on the content while the design of the theme gets out of the
-            way. Beneath the hood, London enjoys the full power of the{" "}
-            <a href="https://docs.ghost.org/api/handlebars-themes/">
-              Ghost Handlebars Theme API
-            </a>{" "}
-            to provide limitless customisation options and dynamic styles.
-          </p>
-          <p>
-            Don't forget to check out the{" "}
-            <a href="https://docs.ghost.org/integrations/">
-              Ghost Integrations Directory
-            </a>{" "}
-            for more ways to integrate Ghost with your favourite services.
-          </p>
+      <article className="page-template no-image">
+        <div className="post-content-body contact row">
+          <div className="contactImg col-6">
+            <figure className="aboutMePic">
+              {/* <Img
+                className="aboutMePic"
+                fluid={data.benchAccounting.childImageSharp.fluid}
+              /> */}
+            </figure>
+          </div>
+          <div className="aboutMe col-6">
+            <h3 className="aboutMeTitle">
+              hi, i am nimesh a conceptual creative based in london
+            </h3>
+            <p>
+              practising in the field of digital products and ui/ux. i enjoy the
+              process of taking an idea from concept to completion through a
+              variety of different methods and journeys. i am constantly
+              striving to better myself, a drive that is reflected in my work.
+              my enjoyment for my craft stems not just from within, but from
+              others too, as i thrive in diverse, social situations. these
+              invaluable experience allow me to expand my understanding of
+              different disciplines and build my skill set.
+            </p>
+            <p>
+              in my downtime, i play with different mediums including; music,
+              still and motion pictures, and designing and developing websites.
+            </p>
+            <p>
+              born in kuwait and brought up in kerala, india, grew into an
+              interactive media designer (imd) at northumbria university in
+              newcastle. now practising at <a href="https://www.tes.com">tes</a>{" "}
+              in london
+            </p>
+            <p>––––––––––––––––</p>
+            <p>
+              hope you have enjoyed looking through my work. if you'd like to
+              dicuss new projects or opportunities; get in touch or find me on{" "}
+              <a href="https://www.linkedin.com/in/nimeshreghunandanan/">
+                linkedin
+              </a>{" "}
+            </p>
+          </div>
         </div>
       </article>
     </Layout>
