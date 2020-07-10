06ff6910543b41ea91dfec2bbfeb522f2e53232c
Nimesh <nimeshnott@hotmail.co.uk>
2020-07-11 00:32:24 +0100

    ignore carousel.css

--- gatsby-config.js
+++ gatsby-config.js
@@ -102,10 +102,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-purgecss`,
       options: {
-        printRejected: true // Print removed selectors and processed file names
+        printRejected: true, // Print removed selectors and processed file names
         // develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
         // whitelist: ['whitelist'], // Don't remove this selector
+        ignore: ["/carousel.css"] // Ignore files/folders
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
