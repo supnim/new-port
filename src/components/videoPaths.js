0e86b8378102cc0cf26a6f1cb939cd9f0ee8eb0b
Nimesh <nimeshnott@hotmail.co.uk>
2020-10-15 13:22:13 +0100

    video linked in description

--- src/components/videoPaths.js
+++ src/components/videoPaths.js
@@ -1,47 +1,85 @@
+import React from "react";
+
 export default [
   {
     id: 1,
-    description: "through the lockdown lens",
+    description: (
+      <a style={{ color: "white" }} href="https://youtu.be/EY3lnnKp8JU">
+        through the lockdown lens
+      </a>
+    ),
     url: "https://www.youtube.com/embed/EY3lnnKp8JU"
   },
   {
     id: 2,
-    description: "dubrovnik, croatia",
+    description: (
+      <a style={{ color: "white" }} href="https://youtu.be/rTwD6_4KZIk">
+        dubrovnik, croatia
+      </a>
+    ),
     url: "https://www.youtube.com/embed/rTwD6_4KZIk"
   },
   {
     id: 3,
-    description: "a day in oxford in 3 second clips",
+    description: (
+      <a style={{ color: "white" }} href="https://youtu.be/zWqSU2Fe_rk">
+        a day in oxford in 3 second clips
+      </a>
+    ),
     url: "https://www.youtube.com/embed/zWqSU2Fe_rk"
   },
   {
     id: 4,
-    desc3ription: "a little camping trip in the lake district",
+    description: (
+      <a style={{ color: "white" }} href="https://youtu.be/UWNQKA1omRY">
+        a little camping trip in the lake district
+      </a>
+    ),
     url: "https://youtube.com/embed/UWNQKA1omRY"
   },
   {
     id: 5,
-    description: "how to kill 17 hours in dubai",
+    description: (
+      <a style={{ color: "white" }} href="https://youtu.be/4H8Q9R_UbHE">
+        how to kill 17 hours in dubai
+      </a>
+    ),
     url: "https://youtube.com/embed/4H8Q9R_UbHE"
   },
   {
     id: 6,
-    description: "celebrating 3 birthdays in berlin",
-    url: "https://youtube.com/embed/yTJ_EMu4eo8/"
+    description: (
+      <a style={{ color: "white" }} href="https://youtu.be/yTJ_EMu4eo8">
+        celebrating 3 birthdays in berlin
+      </a>
+    ),
+    url: "https://youtube.com/embed/yTJ_EMu4eo8"
   },
   {
     id: 7,
-    description: "21st in budapest",
+    description: (
+      <a style={{ color: "white" }} href="https://youtu.be/d98ewXubMB0">
+        21st in budapest
+      </a>
+    ),
     url: "https://youtube.com/embed/d98ewXubMB0"
   },
   {
     id: 8,
-    description: "Dalaro – Empty Apartments",
+    description: (
+      <a style={{ color: "white" }} href="https://youtu.be/Oap3KIQVWZc">
+        Dalaro – Empty Apartments
+      </a>
+    ),
     url: "https://www.youtube.com/embed/Oap3KIQVWZc"
   },
   {
     id: 9,
-    description: "Dalaro – Take my heart ",
+    description: (
+      <a style={{ color: "white" }} href="https://youtu.be/EEJy-wmabiA">
+        Dalaro – Take my heart{" "}
+      </a>
+    ),
     url: "https://www.youtube.com/embed/EEJy-wmabiA"
   }
 ];
