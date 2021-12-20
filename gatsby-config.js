4fb5a0b3d2f893428fe67cf2d5adcbfa16f0e288
Nimesh <nimeshnott@hotmail.co.uk>
2021-12-20 17:59:10 +0000

    retry config

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,11 +98,10 @@ module.exports = {
 				// Defaults used for gatsbyImageData and StaticImage
 				defaults: {},
 				// Set to false to allow builds to continue on image errors
-				failOnError: true,
+				failOnError: false,
 				// deprecated options and their defaults:
 				base64Width: 20,
 				forceBase64Format: `png`, // valid formats: png,jpg,webp
-				useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
 				stripMetadata: true,
 				defaultQuality: 50,
 			},
