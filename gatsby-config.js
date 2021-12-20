e1e335cfd71a7865b01f1114e2b3f687103bc7d7
Nimesh <nimeshnott@hotmail.co.uk>
2021-12-20 17:33:00 +0000

    config update

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,6 +99,12 @@ module.exports = {
 				defaults: {},
 				// Set to false to allow builds to continue on image errors
 				failOnError: true,
+				// deprecated options and their defaults:
+				base64Width: 20,
+				forceBase64Format: `png`, // valid formats: png,jpg,webp
+				useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+				stripMetadata: true,
+				defaultQuality: 50,
 			},
 		},
 		`gatsby-remark-embed-video`,
